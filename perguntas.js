b776b15f809c478f158ff5f58824ddc84f8cfdbf
Rodrigo Almeida de Araújo <00001080749214sp@al.educacao.sp.gov.br>
2024-11-28 21:38:33 -0300

    Alura Perguntas

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Química',
+    'Qual é a fórmula química da água?',
+    'H₂O'
+)
+
+criaCartao(
+    'Conhecimentos gerais',
+    'Qual é o animal conhecido como "rei da selva"?',
+    'O leão'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantos lados tem um triângulo?',
+    '3 lados'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantos segundos há em um minuto?',
+    '60 segundos'
+)
